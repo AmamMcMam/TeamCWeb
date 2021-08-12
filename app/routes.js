ede57a3799311ba56fc6143c2ec054b3b02affec
Thomas Cordall <thomas.cordall@kainos.com>
2021-08-12 11:00:16 +0100

    added basic connection in routes

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+router.get('/addemployee', async (req, res) => {
+    res.render('newemployee');
+});
+
+router.post('/addcity', async (req, res) => {
+        let insertedKey = await citydata.addCity(req.body)
+        res.render('list-cities', { cities: await citydata.getCities()} )
+})
